aa38fcffbbd7c0b36e227a221b9296e45089eb44
jorgeAgoiz <jorgeagoiz@gmail.com>
2023-01-19 20:46:15 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'eslint-config-prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: "./tsconfig.json",
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss'
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>Hola KE ASE</h1>
+    </div>
+  )
+}
+
+export default App
